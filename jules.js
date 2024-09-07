0f77050cde269cc17b75a3fcddd7b52e4f0e247d
Juliet Ugoh <Julietteugoh@gmail.com>
2024-09-07 20:49:37 +0100

    mobile

--- jules.js
+++ jules.js
@@ -1,4 +1,5 @@
 const contactUs = document.getElementById("contactUs");
 contactUs.addEventListener("mouseover", function () {
-  contactUs.innerHTML = "Click Me";
+	contactUs.innerHTML = "Click Me";
+	contactUs.style.background = "green";
 });
